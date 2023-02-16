8ed374deabf2a4648121e51f2a41044a2b4429fd
Michael Oberst <mioberst@gmx.de>
2023-02-16 09:37:55 +0100

    complete notes route

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -5,8 +5,13 @@ const User = require("../models/user.model")
 router.get("/", (req, res, next) => {
     const { _id, paragraphId } = req.query
 
+    // checks if valid parameters provided
+    if (!_id || !paragraphId) {
+        return res.send.status(400).json({ message: "No valid parameters provided." })
+    }
+
     // gets user by user id from DB
-    User.findById( _id )
+    User.findById(_id)
         .then(user => {
 
             // checks if user exists
@@ -31,13 +36,38 @@ router.get("/", (req, res, next) => {
         })
 })
 
-// CREATE new note
+// CREATE, UPDATE, or DELETE notes
 router.post("/", (req, res, next) => {
-    const { _id, paragraphId, note } = req.body
+    const { _id, note } = req.body
+
+    // checks if valid parameters provided
+    if (!_id || !note || !note.paragraphId) {
+        return res.send.status(400).json({ message: "No valid parameters provided." })
+    }
 
     // gets user from DB
     User.findById(_id)
+
+        // creates new notes array
         .then(user => {
-            $addToSet: { notes: note }
+            user.notes = user.notes.filter(oldNote => oldNote.paragraphId !== note.paragraphId)
+            if (note.text) {
+                user.notes.push(note)
+            }
+            return user.notes
+        })
+
+        // updates user data with new notes array
+        .then(notes => {
+            User.findByIdAndUpdate(_id, { notes })
+                .then(() => {
+                    res.status(201).json({ message: "The user notes have been updated." })
+                })
+                .catch(err => {
+                    console.log("Error while updating notes: ", err)
+                    next(err)
+                })
         })
 })
+
+module.exports = router
