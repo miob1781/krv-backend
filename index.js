74d6d3c381a354b751dfaaced09d743f6e619624
Michael Oberst <mioberst@gmx.de>
2023-02-21 10:05:26 +0100

    fix use of morgan

--- index.js
+++ index.js
@@ -18,19 +18,23 @@ const cors = require("cors")
 app.use(cors({
     credentials: true,
     origin: process.env.ORIGIN
-}))        
+}))            
+
+// use logging middleware
+const morgan = require("morgan")
+app.use(morgan("dev"))
 
 // routes
 const isAuthenticated = require("./middleware/jwt.middleware")
 app.use("/auth", require("./routes/user.routes"))
-app.use("/notes", require("./routes/notes.routes"))
-app.use("/lessons", require("./routes/lessons.routes"))
+app.use("/notes", isAuthenticated, require("./routes/notes.routes"))
+app.use("/lessons", isAuthenticated, require("./routes/lessons.routes"))
 
 // error-handling
 app.use((req, res) => {
     // this middleware runs whenever requested page is not available
     res.status(404).json({ errorMessage: "This route is not available." })
-})    
+})            
 
 app.use((err, req, res) => {
     // whenever you call next(err), this middleware will handle the error
@@ -39,19 +43,15 @@ app.use((err, req, res) => {
     // if a token is not valid, send a 401 error
     if (err.name === "UnauthorizedError") {
         res.status(401).json({ message: "invalid token..." })
-    }    
+    }        
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
         res.status(500).json({
             errorMessage: "Internal server error. Check the server console",
-        })    
-    }    
-})    
-
-// use logging middleware
-const morgan = require("morgan")
-app.use(morgan("dev"))
+        })        
+    }        
+})        
 
 // start listening
 const PORT = process.env.PORT
